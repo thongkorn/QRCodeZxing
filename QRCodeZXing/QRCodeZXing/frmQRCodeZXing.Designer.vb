260fb1272dac4365a9c2f0c137a59880abcfbf7b
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-07-25 15:43:04 +0700

    Add files via upload

---
+++ QRCodeZXing/QRCodeZXing/frmQRCodeZXing.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmQRCodeZXing
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.picBarcode = New System.Windows.Forms.PictureBox()
+        Me.txtData = New System.Windows.Forms.TextBox()
+        Me.btnCopy = New System.Windows.Forms.Button()
+        CType(Me.picBarcode, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(36, 21)
+        Me.label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(114, 18)
+        Me.label1.TabIndex = 9
+        Me.label1.Text = "Text to encode:"
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnLoad.Location = New System.Drawing.Point(40, 469)
+        Me.btnLoad.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(376, 32)
+        Me.btnLoad.TabIndex = 2
+        Me.btnLoad.Text = "Load QR Code"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSave.Location = New System.Drawing.Point(40, 429)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(376, 32)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "Save QR Code"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'picBarcode
+        '
+        Me.picBarcode.Location = New System.Drawing.Point(40, 98)
+        Me.picBarcode.Margin = New System.Windows.Forms.Padding(4)
+        Me.picBarcode.Name = "picBarcode"
+        Me.picBarcode.Size = New System.Drawing.Size(376, 301)
+        Me.picBarcode.TabIndex = 6
+        Me.picBarcode.TabStop = False
+        '
+        'txtData
+        '
+        Me.txtData.Location = New System.Drawing.Point(40, 43)
+        Me.txtData.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtData.Name = "txtData"
+        Me.txtData.Size = New System.Drawing.Size(376, 26)
+        Me.txtData.TabIndex = 0
+        '
+        'btnCopy
+        '
+        Me.btnCopy.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCopy.Location = New System.Drawing.Point(40, 512)
+        Me.btnCopy.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnCopy.Name = "btnCopy"
+        Me.btnCopy.Size = New System.Drawing.Size(376, 32)
+        Me.btnCopy.TabIndex = 10
+        Me.btnCopy.Text = "Copy to clipboard"
+        Me.btnCopy.UseVisualStyleBackColor = True
+        '
+        'frmQRCodeZXing
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(455, 557)
+        Me.Controls.Add(Me.btnCopy)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.picBarcode)
+        Me.Controls.Add(Me.txtData)
+        Me.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmQRCodeZXing"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "QR Code with ZXing.Net - coDe bY: Thongkorn Tubtimkrob"
+        CType(Me.picBarcode, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents label1 As Label
+    Private WithEvents btnLoad As Button
+    Private WithEvents btnSave As Button
+    Private WithEvents picBarcode As PictureBox
+    Private WithEvents txtData As TextBox
+    Private WithEvents btnCopy As System.Windows.Forms.Button
+End Class
---
+++ QRCodeZXing/QRCodeZXing/frmQRCodeZXing.vb
@@ -0,0 +1,143 @@
+﻿' / --------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : mailto:thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: Create QRCode (2D) with ZXing.Net.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------
+
+'// ZXing.Net release download.
+'// https://github.com/micjahn/ZXing.Net/releases
+
+Imports System.Drawing.Imaging
+Imports ZXing
+Imports ZXing.Common
+Imports ZXing.QrCode
+Imports System.IO
+
+Public Class frmQRCodeZXing
+
+    Dim strPathImage As String = MyPath(Application.StartupPath)
+
+    Private Sub frmQRCodeZXing_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtData.Text = "www.g2gnet.com/webboard"
+    End Sub
+
+    '// Event when data is changed.
+    Private Sub txtData_TextChanged(sender As Object, e As EventArgs) Handles txtData.TextChanged
+        If String.IsNullOrWhiteSpace(txtData.Text) Then
+            picBarcode.Image = Nothing
+            Return
+        End If
+        '//
+        Dim options As EncodingOptions = New QrCodeEncodingOptions
+        With options
+            .Width = picBarcode.Width
+            .Height = picBarcode.Height
+            .Hints.Add(ZXing.EncodeHintType.CHARACTER_SET, "UTF-8")
+        End With
+
+        Dim objWriter As BarcodeWriter = New BarcodeWriter With {
+                .Format = BarcodeFormat.QR_CODE,
+                .Options = options
+                }
+
+        picBarcode.Image = New Bitmap(objWriter.Write(txtData.Text))
+    End Sub
+
+    '// Save image of QR Code.
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim dlgSaveFile As New SaveFileDialog
+        With dlgSaveFile
+            .Title = "Select images"
+            .Filter = "Image files (*.jpg,*.png,*bmp) | *.jpg; *.png; *.bmp"
+            .FilterIndex = 1
+            .RestoreDirectory = True
+            .InitialDirectory = strPathImage
+        End With
+        '//
+        If dlgSaveFile.ShowDialog() = DialogResult.OK Then
+            '// Saves the Image via a FileStream created by the OpenFile method.
+            Dim fs = CType(dlgSaveFile.OpenFile, FileStream)
+            '// Saves the Image in the appropriate ImageFormat based upon the
+            '// file type selected in the dialog box.
+            Select Case dlgSaveFile.FilterIndex
+                Case 1
+                    picBarcode.Image.Save(fs, ImageFormat.Jpeg)
+                Case 3
+                    picBarcode.Image.Save(fs, ImageFormat.Png)
+                Case 4
+                    picBarcode.Image.Save(fs, ImageFormat.Bmp)
+            End Select
+            fs.Close()
+            MessageBox.Show("QR Code image has been saved.")
+        End If
+    End Sub
+
+    '// Load QR Code and decode it.
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Dim dlgImage As OpenFileDialog = New OpenFileDialog()
+        ' / Open File Dialog
+        With dlgImage
+            '.InitialDirectory = strPath
+            .Title = "Select images"
+            .Filter = "Image files (*.jpg,*.png,*bmp) | *.jpg; *.png; *.bmp"
+            .FilterIndex = 1
+            .RestoreDirectory = True
+        End With
+        ' Select OK after Browse ...
+        If dlgImage.ShowDialog() = DialogResult.OK Then
+            Using FS As IO.FileStream = File.Open(dlgImage.FileName, FileMode.Open)
+                Dim bitmap As Bitmap = New Bitmap(FS)
+                Dim CurrentPicture As Image = CType(bitmap, Image)
+                picBarcode.Image = CurrentPicture
+                '// Decode
+                Dim objReader As BarcodeReader = New BarcodeReader()
+                Dim objResult As Result = objReader.Decode(bitmap)
+                If objResult IsNot Nothing Then
+                    txtData.Text = objResult.Text
+                Else
+                    MessageBox.Show("Cannot decode this image!")
+                End If
+            End Using
+        End If
+    End Sub
+
+    '// Copy to clipboard.
+    Private Sub btnCopy_Click(sender As System.Object, e As System.EventArgs) Handles btnCopy.Click
+        '/ Add it as an image
+        Clipboard.SetImage(picBarcode.Image)
+        '/ Create a JPG on disk and add the location to the clipboard
+        Dim TempName As String = "TempName.jpg"
+        Dim TempPath As String = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.Temp, TempName)
+        Using FS As New System.IO.FileStream(TempPath, IO.FileMode.Create, IO.FileAccess.Write, IO.FileShare.Read)
+            picBarcode.Image.Save(FS, System.Drawing.Imaging.ImageFormat.Jpeg)
+        End Using
+        Dim Paths As New System.Collections.Specialized.StringCollection()
+        Paths.Add(TempPath)
+        Clipboard.SetFileDropList(Paths)
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    ' / Get my project path
+    ' / AppPath = C:\My Project\bin\debug
+    ' / Replace "\bin\debug" with ""
+    ' / Return : C:\My Project\
+    Function MyPath(AppPath As String) As String
+        '/ MessageBox.Show(AppPath);
+        AppPath = AppPath.ToLower()
+        '/ Return Value
+        MyPath = AppPath.Replace("\bin\debug", "").Replace("\bin\release", "").Replace("\bin\x86\debug", "")
+        '// If not found folder then put the \ (BackSlash ASCII Code = 92) at the end.
+        If Microsoft.VisualBasic.Right(MyPath, 1) <> Chr(92) Then MyPath = MyPath & Chr(92)
+    End Function
+
+End Class
+
